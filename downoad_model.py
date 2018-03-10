153f71345f98a640cee95f70479eb8a432de24c2
Jacky Yang <1261972163@qq.com>
2018-03-10 19:44:11 +0800

    Fix some bug

--- downoad_model.py
+++ downoad_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import urllib
-url="https://googledrive.com/host/0B046sNk0DhCDUk9YeklwOFczc0E/fast_rcnn_vgg_voc.model"
+#调试发现在云平台必须如下import才行
+import urllib.request
+#路径地址错误"https://googledrive.com/host/0B046sNk0DhCDUk9YeklwOFczc0E/fast_rcnn_vgg_voc.model"
+url="https://drive.google.com/open?id=0B046sNk0DhCDNW5oMnVGaFdnWkU"
 filename="fast_rcnn_vgg_voc.model"
 #python3方法不在原来的路径了,修改为 urllib.request.urlretrieve
-urllib.urlretrieve(url,filename)
+urllib.request.urlretrieve(url,filename)
