c91585f5992622ba59a9caca26234d2ddc5417e9
Jacky Yang <1261972163@qq.com>
2018-03-10 18:12:42 +0800

    none

--- downoad_model.py
+++ downoad_model.py
@@ -4,4 +4,4 @@ import urllib
 url="https://googledrive.com/host/0B046sNk0DhCDUk9YeklwOFczc0E/fast_rcnn_vgg_voc.model"
 filename="fast_rcnn_vgg_voc.model"
 #python3方法不在原来的路径了,修改为 urllib.request.urlretrieve
-urllib.request.urlretrieve(url,filename)
+urllib.urlretrieve(url,filename)
